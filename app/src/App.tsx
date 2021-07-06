3737142d62fab79115bafe7249d435569ea83d5c
Piriyapong Laopongsit <peawyoyoyin@gmail.com>
2021-07-06 21:21:41 +0800

    add theme selector switch

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,6 +1,60 @@
+import { ThemeSelector } from "./components/ThemeSelector";
+
 function App() {
   return (
     <div>
+      <div>
+        <div>
+          calc
+        </div>
+        <div>
+          THEME
+          <ThemeSelector />
+        </div>
+      </div>
+      <div>
+        399981
+      </div>
+      <div>
+        <div>
+          <div>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>DEL</button>
+            </div>
+        </div>
+        <div>
+          <div>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>+</button>
+            </div>
+        </div>
+        <div>
+          <div>
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>-</button>
+            </div>
+        </div>
+        <div>
+          <div>
+            <button>.</button>
+            <button>0</button>
+            <button>/</button>
+            <button>x</button>
+            </div>
+        </div>
+        <div>
+          <div>
+            <button>RESET</button>
+            <button>=</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ app/src/components/ThemeSelector.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react"
+
+import styles from '../styles/ThemeSelector.module.css';
+
+export function ThemeSelector() {
+  const [theme, setTheme] = useState(0);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.labels}>
+        <label>1</label>
+        <label>2</label>
+        <label>3</label>
+      </div>
+      <div className={styles.switch} onClick={() => setTheme(theme => (theme + 1) % 3)}>
+        <div className={`${styles.dial} ${styles[`dial-${theme}`]}`}></div>
+      </div>
+    </div>
+  ) 
+}
